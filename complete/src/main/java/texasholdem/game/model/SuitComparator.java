52e1fd1d44bf97c3405c8ee1e93fff1107996fcd
Ethan <ethanbinya@aol.com>
2017-12-03 21:45:46 -0500

    Added Javadocs.

--- complete/src/main/java/texasholdem/game/model/SuitComparator.java
+++ complete/src/main/java/texasholdem/game/model/SuitComparator.java
@@ -3,9 +3,17 @@ package texasholdem.game.model;
 import java.util.Comparator;
 
 /**
- * Created by Ethan on 11/13/2017.
+ * Comparator for the Card class. This compares Cards by Suit, then, if Suits are equal, by value.
+ * @see java.util.Comparator
+ * @see ValueComparator
  */
-class SuitComparator implements Comparator<Card> {
+public class SuitComparator implements Comparator<Card> {
+    /**
+     * This compares Cards by Suit, then, if Suits are equal, by value.
+     * @param a Card
+     * @param b Card
+     * @return positive int when a > b, negative int when a < b, or 0 when a equals b.
+     */
     @Override
     public int compare(Card a, Card b){
         int ret = b.getSuitValue() - a.getSuitValue();
